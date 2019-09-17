95c1e2f8af567b9ee91fdc26a88a6d2df9a521da
djl <jiaolongdu@qq.com>
2019-09-17 08:50:22 +0800

    设置仓库忽略规则

--- ros/catkin_ws/build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jiaolongdu/djl/graduate_project/ros/catkin_ws/src/beginner_tutorials
-# Build directory: /home/jiaolongdu/djl/graduate_project/ros/catkin_ws/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
